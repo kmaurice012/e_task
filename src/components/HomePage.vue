2d563677dc8b47af5b9e70aee740657fa1433cf0
kmaurice012 <kingmaurice012@gmail.com>
2023-02-23 17:26:54 +0300

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>This is the Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+h1{
+  font-size: 40px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "../components/HomePage.vue";
+
+const routes = [
+    {
+        path: "/home",
+        name: "HomePage",
+        component: HomePage,
+    },
+    // {
+    //     path: "/about",
+    //     name: "About",
+    //     component: About,
+    // },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
